2502f011ff4f424f9e29161567fb99f749171b20
Katerynaa91 <157005316+Katerynaa91@users.noreply.github.com>
2024-11-16 16:21:30 +0200

    Task 1. Bash. Http Status Codes

--- Task1/urls.sh
+++ Task1/urls.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+websites=("https://google.com/" "https://weather.com/" "https://facebook.com/")
+
+log_file="logs.log"
+
+> "$log_file"
+
+for website in "${websites[@]}"
+do
+        timestamp=$(date +"%Y-%m-%d %H:%M%S")
+
+        status_code=$(curl -k -I -o /dev/null -s -w "%{http_code}" -L "$website")
+
+        if [ "$status_code" -eq 200 ]; then
+                echo "[$timestamp] $website is UP $status_code" | tee -a "$log_file"
+        else
+                echo "[$timestamp] $website is DOWN $status_code" | tee -a "$log_file"
+        fi
+done
